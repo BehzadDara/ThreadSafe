140749dbb0db0504ca7b3ce6713f31271bd53673
behzad dara <Behzad.dara.99@gmail.com>
2025-05-20 17:27:27 +0330

    Add project files.

---
+++ ThreadSafe/NumberManagement.cs
@@ -0,0 +1,76 @@
+﻿namespace ThreadSafe;
+
+public class NumberManagement
+{
+    private int _number = 0; 
+    private readonly object _lock = new(); 
+    private readonly SemaphoreSlim _semaphore = new(1);
+
+    public void Add(int number)
+    {
+        _number += number;
+        Console.WriteLine($"After add {number} result is {_number}");
+    }
+
+    public void Subtract(int number)
+    {
+        _number -= number;
+        Console.WriteLine($"After subtract {number} result is {_number}");
+    }
+
+    public void AddLock(int number)
+    {
+        lock (_lock)
+        {
+            _number += number;
+            Console.WriteLine($"After add lock {number} result is {_number}");
+        }
+    }
+
+    public void SubtractLock(int number)
+    {
+        lock (_lock)
+        {
+            _number -= number;
+            Console.WriteLine($"After subtract lock {number} result is {_number}");
+        }
+    }
+
+    public async Task AddSemaphore(int number)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            _number += number;
+            Console.WriteLine($"After add semaphore {number} result is {_number}");
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task SubtractSemaphore(int number)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            _number -= number;
+            Console.WriteLine($"After subtract semaphore {number} result is {_number}");
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public void AddInterLocked(int number)
+    {
+        Interlocked.Add(ref _number, number);
+    }
+
+    public void SubtractInterLocked(int number)
+    {
+        Interlocked.Add(ref _number, -1 * number);
+    }
+}
---
+++ ThreadSafe/Program.cs
@@ -0,0 +1,29 @@
+﻿using ThreadSafe;
+
+// not threadsafe
+var numberManagement1 = new NumberManagement();
+numberManagement1.Add(5);
+numberManagement1.Add(6);
+numberManagement1.Subtract(7);
+Console.WriteLine("-----------------------------------");
+
+// lock (1 thread - in process scope - no async await support - lightweight - simple)
+var numberManagement2 = new NumberManagement();
+numberManagement2.AddLock(5);
+numberManagement2.AddLock(6);
+numberManagement2.SubtractLock(7);
+Console.WriteLine("-----------------------------------");
+
+// semaphore slim (n thread - in process scope - with async await support - lightweight compared to semaphore - not simple)
+var numberManagement3 = new NumberManagement();
+await numberManagement3.AddSemaphore(5);
+await numberManagement3.AddSemaphore(6);
+await numberManagement3.SubtractSemaphore(7);
+Console.WriteLine("-----------------------------------");
+
+// inter locked (low overhead - non block - only numeric updates)
+var numberManagement4 = new NumberManagement();
+numberManagement4.AddInterLocked(5);
+numberManagement4.AddInterLocked(6);
+numberManagement4.SubtractInterLocked(7);
+Console.WriteLine("-----------------------------------");
