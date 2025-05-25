e5ceb745cc323adc2956e91d738645a9de1aa632
Behzad Dara <88719644+BehzadDara@users.noreply.github.com>
2025-05-26 00:30:04 +0330

    Update Program.cs

--- ThreadSafe/Program.cs
+++ ThreadSafe/Program.cs
@@ -27,3 +27,54 @@ numberManagement4.AddInterLocked(5);
 numberManagement4.AddInterLocked(6);
 numberManagement4.SubtractInterLocked(7);
 Console.WriteLine("-----------------------------------");
+
+/*
+
+class Program
+{
+    /*
+     initialCount is the number of resource accesses that will be allowed immediately. 
+    Or, in other words, it is the number of times Wait can be called 
+    without blocking immediately after the semaphore was instantiated.
+
+    maximumCount is the highest count the semaphore can obtain. 
+    It is the number of times Release can be called 
+    without throwing an exception assuming initialCount count was zero. 
+    If initialCount is set to the same value as maximumCount 
+    then calling Release immediately after the semaphore was instantiated will throw an exception.
+     */
+
+    public static int initialCount = 3;
+    public static int maximumCount = 5;
+    public static Semaphore semaphore = new(initialCount, maximumCount);
+
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var threadObject = new Thread(Process)
+            {
+                Name = $"Thread: {i}"
+            };
+
+            threadObject.Start();
+        }
+    }
+
+    private static void Process()
+    {
+        var threadName = Thread.CurrentThread.Name;
+
+        Console.WriteLine($"{threadName} is waiting to enter the critical section.");
+        semaphore.WaitOne();
+
+        Console.WriteLine($"{threadName} is inside the critical section now.");
+        Thread.Sleep(1000);
+
+        Console.WriteLine($"{threadName} is releasing the critical section.");
+        semaphore.Release();
+    }
+
+}
+
+*/
